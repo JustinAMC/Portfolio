3cd070fe802347f352a96e0c55ae7608866716ea
JustinAMC <justinamcruz@outlook.com>
2023-08-17 23:04:13 -0400

    added most of the stuff for sending emails

---
+++ index/main.js
@@ -0,0 +1,16 @@
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "justinmatosc@gmail.com",
+        Password : "Grootgeheim1!2",
+        To : 'justinmatosc@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Submission",
+        Body : "Name: " + document.getElementById("name").value
+        + "<br> Email: " + document.getElementById("email").value
+        + " <br> Phone Number: " + document.getElementById("phone").value
+        + "<br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Message Sent Successfully. Talk to you soon!")
+    );
+}
